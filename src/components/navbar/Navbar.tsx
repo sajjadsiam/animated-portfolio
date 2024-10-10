2e1da375412ce7a6297ddfc21fdeac77f8846ff4
Sajjad Siam <sajjadhosensiam@gmail.com>
2024-10-11 02:09:01 +0600

    xcc
    cdcx

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import "./navbar.scss"
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+
+    {/* sidebar */}
+
+    <div className="wrapper">
+      <span>Sajjad Siam</span>
+      <div className="social">
+        <a href="https://www.facebook.com/sajjadsiam"> <img src="/facebook.png" alt="" /> </a>
+        <a href="https://www.facebook.com/sajjadsiam"> <img src="/instagram.png" alt="" /> </a>
+        <a href="https://www.facebook.com/sajjadsiam"> <img src="/youtube.png" alt="" /> </a>
+        <a href="https://www.facebook.com/sajjadsiam"> <img src="/dribbble.png" alt="" /> </a>
+      </div>
+    </div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
